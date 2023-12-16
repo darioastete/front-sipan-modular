789e1fa5ff7b788bca662418d2b9c6e30e522d28
userwinwsl <castete@synaptec.com>
2023-12-16 05:02:59 -0500

    chore: inicial project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from "./views/layout/login/login.component";
+import { HomeComponent } from "./views/dashboard/home/home.component";
+import { DashboardComponent } from "./views/dashboard/dashboard.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'login', component: LoginComponent
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    children : [
+      { path:'home', component: HomeComponent }
+    ]
+  }
+];
